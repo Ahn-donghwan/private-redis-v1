220a965cce9c7eea95d84b1383d6656978fe1bb5
Ahn-donghwan <ddh1713flrwls1713@gmail.com>
2025-05-15 20:20:16 +0900

    chores: default settings

---
+++ src/main/java/com/unionclass/privateredisv1/PrivateRedisV1Application.java
@@ -0,0 +1,13 @@
+package com.unionclass.privateredisv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateRedisV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivateRedisV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/unionclass/privateredisv1/PrivateRedisV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unionclass.privateredisv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateRedisV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
